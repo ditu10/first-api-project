2dbde116046f6c504fc4767c68530b98b2bcacbf
ditu10 <oyasiul.islam_ditu@yahoo.com>
2021-08-29 01:07:54 +0600

    food Bank added

---
+++ js/foodDetails.js
@@ -0,0 +1,33 @@
+window.onload = function () {
+    var url = document.location.href,
+        params = url.split('?')[1].split('&'),
+        data = {}, tmp;
+    for (var i = 0, l = params.length; i < l; i++) {
+         tmp = params[i].split('=');
+         data[tmp[0]] = tmp[1];
+    }
+    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${data.name}`)
+        .then(res => res.json())
+        .then(data => showDetails(data))
+    
+}
+
+
+const showDetails = (data) => {
+    let food = data.meals[0]
+    const food_details = document.getElementById('food-details')
+    const divLeft = document.createElement('div')
+    const divright = document.createElement('div')
+    divLeft.classList.add('w-5/6', 'mx-auto' , 'py-5')
+    divright.classList.add('w-5/6', 'mx-auto' , 'py-5')
+    divLeft.innerHTML = `
+    <h3 class="text-3xl py-4 text-center font-semibold text-yellow-600">${food.strMeal}</h3>
+    <img src="${food.strMealThumb}">
+    `
+    divright.innerHTML = `
+    <h2 class="pt-14 text-3xl underline text-yellow-900">How to Cook:</h2>
+    <p class="pt-8 text-xl text-yellow-600">${food.strInstructions}</p>
+    `
+    food_details.appendChild(divLeft);
+    food_details.appendChild(divright);
+}
---
+++ js/searchfood.js
@@ -0,0 +1,75 @@
+document.getElementById('search-btn').addEventListener('click', function () {
+    const searchBox = document.getElementById('search-box');
+    const searchText = searchBox.value;
+    document.getElementById('loading').style.display = 'block';
+    loadFood(searchText);
+
+    searchBox.value = '';
+})
+
+const loadFood = (food) => {
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${food}`
+    // console.log(url)
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayFoods(data))
+    
+}
+
+const displayFoods = foods => {
+    // console.log(foods.meals)
+    document.getElementById('loading').style.display = 'none';
+    deleteChild();
+    if (document.getElementById('noFood').childElementCount!=0) {
+        const food_section = document.getElementById('noFood')
+        food_section.removeChild(food_section.children[0])
+    }
+    if (foods.meals == null) {
+        const food_section = document.getElementById('noFood')
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <p class="text-center text-2xl text-red-400">No search item found!!</p>
+        `;
+        
+        food_section.appendChild(div);
+        return
+    }
+    
+    foods.meals.forEach(food => {
+        console.log(food)
+        const food_section = document.getElementById('foods-section');
+        const div = document.createElement('div');
+        div.classList.add('w-5/6', 'mx-auto' , 'py-5')
+        div.innerHTML = `
+        <img onclick="displayAllItems('${food.idMeal}')" src="${food.strMealThumb}">
+        <h3 onclick="displayAllItems('${food.idMeal}')" class="text-xl text-center text-yellow-600 font-semibold">${food.strMeal}</h3>
+        `
+        food_section.appendChild(div)
+    });
+}
+
+const deleteChild = () => {
+    const food_section = document.getElementById('foods-section');
+
+    const count = food_section.childElementCount;
+    
+    for (let i = 0; i < count; i++){
+        food_section.removeChild(food_section.children[0])
+    }
+}
+
+const displayAllItems = foodId => {
+    // fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${foodId}`)
+    //     .then(res => res.json())
+    //     .then(data => console.log(data))
+    const link = foodId;
+    const url = 'http://127.0.0.1:5500/foodDetails.html?name=' + encodeURIComponent(link);
+    // window.location.href = url;
+    window.open(url,'_blank');
+    // showFoodDetails()  
+    // document.getElementById('food-details').innerText = 'Hello foodies'
+}
+
+const showFoodDetails = () => {
+    
+}
