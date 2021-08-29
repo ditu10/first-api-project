ac9f4cbc97de7b751719eb027295d23c269a041c
ditu10 <oyasiul.islam_ditu@yahoo.com>
2021-08-29 16:36:54 +0600

    individual food details

--- js/foodDetails.js
+++ js/foodDetails.js
@@ -15,6 +15,7 @@ window.onload = function () {
 
 const showDetails = (data) => {
     let food = data.meals[0];
+    console.log(food)
     document.getElementById('loading').style.display = "none";
     const food_details = document.getElementById('food-details')
     const divLeft = document.createElement('div')
@@ -22,13 +23,59 @@ const showDetails = (data) => {
     divLeft.classList.add('w-5/6', 'mx-auto' , 'py-5')
     divright.classList.add('w-5/6', 'mx-auto' , 'py-5')
     divLeft.innerHTML = `
-    <h3 class="text-3xl py-4 text-center font-semibold text-yellow-600">${food.strMeal}</h3>
+    <h3 class="text-3xl py-4 text-center font-semibold text-yellow-500">${food.strMeal}</h3>
     <img src="${food.strMealThumb}">
     `
+    // divright.classList.add('col-span-2')
+    // divright.classList.add('overflow-y-auto');
     divright.innerHTML = `
-    <h2 class="pt-14 text-3xl underline text-yellow-900">How to Cook:</h2>
-    <p class="pt-8 text-xl text-yellow-600">${food.strInstructions}</p>
+        <h3 class="text-xl pt-5">Area: <span class="text-yellow-500 text-xl">${food.strArea}</span> </h3>
+        <h3 class="text-xl pt-5">Category: <span class="text-yellow-500 text-xl">${food.strCategory}</span> </h3>
+        <h3 class="text-2xl text-yellow-500 pt-8">Recipie details:</h3>
+            
+        <p class="text-xl overflow-y-auto h-80 pt-5">${food.strInstructions}</p>
     `
     food_details.appendChild(divLeft);
-    food_details.appendChild(divright);
+    // food_details.appendChild(divright);
+
+    showIngradients(food)
+}
+
+
+
+const showIngradients = (food) => {
+    console.log(food)
+    const div = document.createElement('div');
+    div.classList.add('col-span-2','pt-8')
+    const innerDiv = document.createElement('div');
+    innerDiv.classList.add('grid','grid-cols-3','gap-8')
+
+    
+    
+    for (let count = 1; count < 20; count++){
+        let ingradient = `strIngredient${count}`;
+        if (food[ingradient] != '') {
+            const package = document.createElement('div')
+            package.classList.add('mx-auto','shadow-lg','py-4')
+            
+            const ingredQuantity = `strMeasure${count}`;
+            const ingredName = food[ingradient];
+
+           
+            package.innerHTML = `
+            <img src="https://www.themealdb.com/images/ingredients/${ingredName}.png">
+            <p class = "text-xl text-center text-yellow-500">${food[ingredQuantity]} ${ingredName}</p>
+            `
+            
+            innerDiv.appendChild(package)
+            
+        }
+    }
+    // innerDiv.classList.add('my-4')
+    div.appendChild(innerDiv)
+    const food_details = document.getElementById('food-details');
+    
+   
+    
+    food_details.appendChild(div)
 }
--- js/searchfood.js
+++ js/searchfood.js
@@ -1,6 +1,8 @@
 document.getElementById('search-btn').addEventListener('click', function () {
     const searchBox = document.getElementById('search-box');
     const searchText = searchBox.value;
+    document.getElementById('foods-section').textContent = '';
+    document.getElementById('noFood').textContent = '';
     document.getElementById('loading').style.display = 'block';
     loadFood(searchText);
 
@@ -19,16 +21,14 @@ const loadFood = (food) => {
 const displayFoods = foods => {
     // console.log(foods.meals)
     document.getElementById('loading').style.display = 'none';
-    deleteChild();
-    if (document.getElementById('noFood').childElementCount!=0) {
-        const food_section = document.getElementById('noFood')
-        food_section.removeChild(food_section.children[0])
-    }
+    
+    document.getElementById('noFood').textContent = '';
+
     if (foods.meals == null) {
         const food_section = document.getElementById('noFood')
         const div = document.createElement('div');
         div.innerHTML = `
-        <p class="text-center text-2xl text-red-400">No search item found!!</p>
+        <p class="text-center text-xl text-red-600">No search item found!!</p>
         `;
         
         food_section.appendChild(div);
@@ -42,33 +42,21 @@ const displayFoods = foods => {
         div.classList.add('w-5/6', 'mx-auto' , 'py-5')
         div.innerHTML = `
         <img onclick="displayAllItems('${food.idMeal}')" src="${food.strMealThumb}">
-        <h3 onclick="displayAllItems('${food.idMeal}')" class="text-xl text-center text-yellow-600 font-semibold">${food.strMeal}</h3>
+        <h3 onclick="displayAllItems('${food.idMeal}')" class="text-xl text-center text-yellow-500 font-semibold">${food.strMeal}</h3>
         `
         food_section.appendChild(div)
     });
 }
 
-const deleteChild = () => {
-    const food_section = document.getElementById('foods-section');
-
-    const count = food_section.childElementCount;
-    
-    for (let i = 0; i < count; i++){
-        food_section.removeChild(food_section.children[0])
-    }
-}
 
 const displayAllItems = foodId => {
-    // fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${foodId}`)
-    //     .then(res => res.json())
-    //     .then(data => console.log(data))
+    
     const code = foodId;
     const link = window.location.href.slice(0, window.location.href.length - 15);
     const url = `${link}foodDetails.html?name=` + encodeURIComponent(code);
-    // window.location.href = url;
+    
     window.open(url,'_blank');
-    // showFoodDetails()  
-    // document.getElementById('food-details').innerText = 'Hello foodies'
+    
 }
 
 const showFoodDetails = () => {
